91397aec6f16d902078c680d6d8f3b989748ec57
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-01-12 14:10:10 +0700

    change 0.5 to 1

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -24,7 +24,7 @@ $(function() {
 				$("#receiver").val('');
 				loader.addClass("hidden");
 				swal("Success",
-				  "0.5 TOMO is successfully transfered to " + receiver +" in Tx<br /><a href='https://explorer.tomocoin.io/#/tx/" + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
+				  "1 TOMO is successfully transfered to " + receiver +" in Tx<br /><a href='https://explorer.tomocoin.io/#/tx/" + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
 				  "success"
 				);
 				grecaptcha.reset();
